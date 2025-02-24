e617837b8c665876340bb040139ea833fdbb4248
Samuel Asp <samuelasp96@gmail.com>
2025-02-24 19:36:04 +0100

    Update help menu and hide functions

--- src/functions.h
+++ src/functions.h
@@ -2,29 +2,14 @@
 
 #include "voting_round.h"
 
-/* -------------- Printing active screen -------------- */
+/* -------------- Menu strings -------------- */
 auto getActiveMenuString(std::optional<VotingRound> const& voting_round, bool const show_intro_screen)->std::string;
-void printActiveMenu(std::optional<VotingRound> const& voting_round, bool const show_intro_screen);
-
-/* -------------- Printing miscellaneous -------------- */
 auto getHelpString()->std::string;
-void printHelp();
 
 /* -------------- Command line inputs -------------- */
 auto getKey() -> char;
-auto getLine() -> std::string;
 auto getConfirmation() -> bool;
-auto getMultipleFileNames() -> std::vector<std::string>;
 auto continueWithoutSaving(std::optional<VotingRound> const& voting_round, std::string const& str) -> bool;
-
-/* -------------- Localization -------------- */
-auto fixSwedish(std::string const& str) -> std::string;
-
-/* -------------- String-to-object parsing -------------- */
-auto parseItems(std::vector<std::string> const& lines) -> Items;
-
-/* -------------- Verifications -------------- */
-auto verifyFilesExist(std::vector<std::string> const& file_names) -> bool;
 
 /* -------------- Menu alternatives -------------- */
 auto vote(std::optional<VotingRound>& voting_round, Option option)->std::string;

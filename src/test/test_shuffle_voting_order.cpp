f408bac8adab4cac4f1499d111fd9c20fb9de72c
Samuel Asp <samuelasp96@gmail.com>
2025-02-25 20:28:23 +0100

    Extract index pairs to separate class
    With upcoming alternative voting formats, a voting round
    should have only one at a time. But items, seed, and votes
    will remain the same. Thus, only index pairs and functions
    specific to it are extracted into the implementation-specific
    VotingRound::ScoreBased structure. Index pairs are no longer
    obtainable outside of VotingRound.
    
    To facilitate this change, tests which relied on obtaining
    index pairs instead either make use of the votes - which
    will have the same information, but require the voting round
    to be completed to be of use - or are removed as they were
    testing implementation-specific code.
    
    Since vote pruning is specific to the score-based voting format,
    and since pruning should not occur during a voting round anyway,
    it now only occurs during voting round creation. Tests which
    verify this feature after creation are removed.
    
    Also:
    - Some test helper vector operators are moved to testing.h

--- src/test/test_shuffle_voting_order.cpp
+++ src/test/test_shuffle_voting_order.cpp
@@ -5,35 +5,37 @@ int main() {
 	auto voting_round = VotingRound::create(getNItems(8), VotingFormat::Full, 123456);
 	voting_round.value().shuffle();
 
-	ASSERT_EQ(voting_round.value().indexPairs(), IndexPairs{
-		IndexPair{0, 6},
-		IndexPair{0, 3},
-		IndexPair{6, 7},
-		IndexPair{2, 7},
-		IndexPair{3, 7},
-		IndexPair{0, 1},
-		IndexPair{0, 7},
-		IndexPair{3, 5},
-		IndexPair{3, 4},
-		IndexPair{1, 2},
-		IndexPair{2, 5},
-		IndexPair{4, 5},
-		IndexPair{0, 4},
-		IndexPair{0, 2},
-		IndexPair{4, 6},
-		IndexPair{5, 7},
-		IndexPair{0, 5},
-		IndexPair{2, 6},
-		IndexPair{5, 6},
-		IndexPair{4, 7},
-		IndexPair{2, 4},
-		IndexPair{2, 3},
-		IndexPair{1, 6},
-		IndexPair{1, 4},
-		IndexPair{3, 6},
-		IndexPair{1, 3},
-		IndexPair{1, 5},
-		IndexPair{1, 7} });
+	while (voting_round.value().vote(Option::A));
+
+	ASSERT_EQ(voting_round.value().votes(), Votes{
+		Vote{0, 6, Option::A},
+		Vote{0, 3, Option::A},
+		Vote{6, 7, Option::A},
+		Vote{2, 7, Option::A},
+		Vote{3, 7, Option::A},
+		Vote{0, 1, Option::A},
+		Vote{0, 7, Option::A},
+		Vote{3, 5, Option::A},
+		Vote{3, 4, Option::A},
+		Vote{1, 2, Option::A},
+		Vote{2, 5, Option::A},
+		Vote{4, 5, Option::A},
+		Vote{0, 4, Option::A},
+		Vote{0, 2, Option::A},
+		Vote{4, 6, Option::A},
+		Vote{5, 7, Option::A},
+		Vote{0, 5, Option::A},
+		Vote{2, 6, Option::A},
+		Vote{5, 6, Option::A},
+		Vote{4, 7, Option::A},
+		Vote{2, 4, Option::A},
+		Vote{2, 3, Option::A},
+		Vote{1, 6, Option::A},
+		Vote{1, 4, Option::A},
+		Vote{3, 6, Option::A},
+		Vote{1, 3, Option::A},
+		Vote{1, 5, Option::A},
+		Vote{1, 7, Option::A} });
 
 	return 0;
 }

6143870f2ca7ce0f69e054ac014df41b2924b2c0
Samuel Asp <samuelasp96@gmail.com>
2025-03-05 17:31:19 +0100

    Move source_location to last argument

--- src/test/testing.h
+++ src/test/testing.h
@@ -8,67 +8,71 @@
 /* -------------- Test definitions -------------- */
 #define RUN_TEST_IF_ARGUMENT_EQUALS(test_name) if (std::string{ argv[1] } == #test_name) { test_name(); return 0; }
 
-/* -------------- Assertions details -------------- */
-void assert_true(std::source_location const& location, bool val);
+namespace detail
+{
+
+void assert_true(bool val, std::source_location const& location);
 
 template<typename T>
-void assert_eq(std::source_location const& location, T const& a, T const& b) {
-	assert_true(location, a == b);
+void assert_eq(T const& a, T const& b, std::source_location const& location) {
+	assert_true(a == b, location);
 }
 template<typename T>
-void assert_ne(std::source_location const& location, T const& a, T const& b) {
-	assert_true(location, a != b);
+void assert_ne(T const& a, T const& b, std::source_location const& location) {
+	assert_true(a != b, location);
 }
 
 template<typename T>
-void assert_eq(std::source_location const& location, std::optional<T> const& a, std::optional<T> const& b) {
+void assert_eq(std::optional<T> const& a, std::optional<T> const& b, std::source_location const& location) {
 	if (a == b) {
 		return;
 	}
 	if (!a.has_value()) {
-		assert_true(location, false);
+		assert_true(false, location);
 		return;
 	}
 	if (!b.has_value()) {
-		assert_true(location, false);
+		assert_true(false, location);
 		return;
 	}
-	assert_true(location, false);
+	assert_true(false, location);
 }
 template<typename T>
-void assert_ne(std::source_location const& location, std::optional<T> const& a, std::optional<T> const& b) {
+void assert_ne(std::optional<T> const& a, std::optional<T> const& b, std::source_location const& location) {
 	if (!a.has_value() && !b.has_value()) {
-		assert_true(location, false);
+		assert_true(false, location);
 		return;
 	}
 	if (a.has_value() && b.has_value()) {
-		assert_ne(location, a.value() == b.value());
+		assert_ne(a.value() == b.value(), location);
 	}
 }
 template<typename T>
-void assert_eq(std::source_location const& location, std::optional<T> const& a, std::nullopt_t const& b) {
-	assert_true(location, !a.has_value());
+void assert_eq(std::optional<T> const& a, std::nullopt_t const& b, std::source_location const& location) {
+	assert_true(!a.has_value(), location);
 }
 template<typename T>
-void assert_ne(std::source_location const& location, std::optional<T> const& a, std::nullopt_t const& b) {
-	assert_true(location, a.has_value());
+void assert_ne(std::optional<T> const& a, std::nullopt_t const& b, std::source_location const& location) {
+	assert_true(a.has_value(), location);
 }
-void assert_eq(std::source_location const& location, std::nullopt_t const& a, std::nullopt_t const& b);
-void assert_ne(std::source_location const& location, std::nullopt_t const& a, std::nullopt_t const& b);
+void assert_eq(std::nullopt_t const& a, std::nullopt_t const& b, std::source_location const& location);
+void assert_ne(std::nullopt_t const& a, std::nullopt_t const& b, std::source_location const& location);
+
+} // namespace detail
 
 /* -------------- Assertions to use in tests -------------- */
 template<typename A, typename B>
 void ASSERT_EQ(A const& a, B const& b, std::source_location const location = std::source_location::current()) {
-	assert_eq(location, a, b);
+	detail::assert_eq(a, b, location);
 }
 template<typename T>
 void ASSERT_NE(T const& a, T const& b, std::source_location const location = std::source_location::current()) {
-	assert_ne(location, a, b);
+	detail::assert_ne(a, b, location);
 }
 void ASSERT_TRUE(bool a, std::source_location const location = std::source_location::current());
 void ASSERT_FALSE(bool a, std::source_location const location = std::source_location::current());
 
-/* -------------- Test helpers -------------- */
+/* -------------- Miscellaneous test helpers -------------- */
 template<typename T>
 auto operator+(std::vector<T> const& vec, T&& str) -> decltype(auto) {
 	auto new_vec = vec;

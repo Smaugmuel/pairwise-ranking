c792fd1ad95f24c23a679cb1a1fd426cab589891
Samuel Asp <samuelasp96@gmail.com>
2025-03-28 22:02:04 +0100

    Print asserted values in tests
    Strings and integer values are now printed when using
    ASSERT_EQ and ASSERT_NE, to make failing tests easier
    to debug.

--- src/test/testing.cpp
+++ src/test/testing.cpp
@@ -2,41 +2,30 @@
 
 #include <iostream>
 
-namespace
+namespace detail
 {
 
 void assertion_failure(std::string const& error, std::source_location const& location) {
-	std::cout << "Assertion failed " << location.file_name() << ":" << location.line() << ": " << error << 
+	std::cout << "Assertion failed " << location.file_name() << ":" << location.line() << ": " << error <<
 		". In " << location.function_name() << "()" << std::endl;
 	exit(1);
 }
 
-} // namespace
-
-namespace detail
-{
-
-void assert_true(bool val, std::source_location const& location) {
-	if (!val) {
-		assertion_failure("", location);
-	}
-}
-
-void assert_eq(std::nullopt_t const& a, std::nullopt_t const& b, std::source_location const& location) {
-	assert_true(true, location);
-}
-
-void assert_ne(std::nullopt_t const& a, std::nullopt_t const& b, std::source_location const& location) {
-	assert_true(false, location);
+auto equalityStr(std::string const& a, std::string const& b, std::string const& eq_str) -> std::string {
+	return "Expected " + eq_str + " between " + a + " and " + b;
 }
 
 } // namespace detail
 
 void ASSERT_TRUE(bool a, std::source_location const location) {
-	detail::assert_true(a, location);
+	if (!a) {
+		detail::assertion_failure("", location);
+	}
 }
 void ASSERT_FALSE(bool a, std::source_location const location) {
-	detail::assert_true(!a, location);
+	if (a) {
+		detail::assertion_failure("", location);
+	}
 }
 
 auto getNItems(size_t n) -> std::vector<std::string> {

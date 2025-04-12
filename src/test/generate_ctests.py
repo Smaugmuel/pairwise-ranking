aae7fb0cc740e6014caa33edeb2b1660bead2b17
Samuel Asp <samuelasp96@gmail.com>
2025-04-12 15:43:45 +0200

    Simplify test cases in CMakeLists
    Test suites and test cases are now defined without
    needing to explicitly populate and reset a list of
    test cases. Instead, the test cases list is defined
    as multiple arguments to addTestSuite.
    
    Test generation script is updated to conform to this.

--- src/test/generate_ctests.py
+++ src/test/generate_ctests.py
@@ -1,80 +1,78 @@
 import glob, os, re
 
-os.chdir(os.path.dirname(__file__))
-
-test_suite_files = glob.glob("test_*.cpp")
-test_suites = [ name[:-4] for name in test_suite_files]
-
-with open("CMakeLists.txt", 'r') as cmake_file:
-	cmake_file_content = cmake_file.readlines()
-
 def findAllTestsInFile(file_name):
 	with open(file_name, 'r') as file:
 		file_content = file.read()
 		return [test[5:-2] for test in re.findall(r'void .*\(\)', file_content)]
 
-def findLineInList(list_to_search, line_to_find):
+def findLineIndexOfMatch(list_to_search, element) -> int:
 	for num, line in enumerate(list_to_search, 1):
-		if line_to_find in line:
-			return num
-	print("Failed to find " + line_to_find)
+		if element in line:
+			return num - 1
+	print("No match found for '" + element + "'")
 	return -1
 
-def findPreviousTestCaseList(list_to_search, line_number):
-	for num, line in reversed([x for x in enumerate(list_to_search, 1) ]):
-		if num >= line_number:
+def findLineIndexOfNextClosingParenthesis(list_to_search, starting_index):
+	for num, line in enumerate(list_to_search, 1):
+		if num < starting_index + 1:
 			continue
 		if "addTestSuite" in line:
 			break
-		if "list(APPEND test_cases" in line:
-			return num
-	print("No test_cases list found")
+		if ")" in line:
+			return num - 1
+	print("No match found for ')'")
 	return -1
 
-def findNextReturnInTestSuite(list_to_search, line_number):
+def findLineIndexOfNextReturn(list_to_search, starting_index):
 	for num, line in enumerate(list_to_search, 1):
-		if num < line_number:
+		if num < starting_index + 1:
 			continue
 		if "return true" in line:
-			return num
-	print("No return found")
+			return num - 1
+	print("No match found for 'return'")
 	return -1
 
-#### Update CMakeLists.txt #####
-for test_suite in test_suites:
-	add_test_suite_line = findLineInList(cmake_file_content, test_suite)
-	test_cases_list_line = findPreviousTestCaseList(cmake_file_content, add_test_suite_line)
+def updateCMakeLists(test_suite):
+	with open("CMakeLists.txt", 'r') as file:
+		cmake_file_data = file.readlines()
 
-	# Note: Conversion to index kept for clarity
-	test_cases_start_index = (test_cases_list_line - 1) + 1
-	test_cases_end_index   = (add_test_suite_line  - 1) - 2
+	# Find range of existing CTest cases
+	tests_start_index = findLineIndexOfMatch(cmake_file_data, test_suite) + 1
+	tests_end_index = findLineIndexOfNextClosingParenthesis(cmake_file_data, tests_start_index) - 1
 
-	del cmake_file_content[test_cases_start_index:test_cases_end_index+1]
-
-	cmake_file_content[test_cases_start_index:test_cases_start_index] = [
+	# Replace exiting CTest cases with tests found in source code
+	del cmake_file_data[tests_start_index:tests_end_index+1]
+	cmake_file_data[tests_start_index:tests_start_index] = [
 		'\t' + x + '\n'
 		for x in findAllTestsInFile(test_suite + ".cpp")
 	]
-	
+
 	with open("CMakeLists.txt", "w") as file:
-		file.write("".join(cmake_file_content))
-		
-##### Update test suite file #####
-for test_suite in test_suites:
-	with open(test_suite + ".cpp", 'r') as test_suite_file:
-		test_suite_file_content = test_suite_file.readlines()
-	run_tests_line = findLineInList(test_suite_file_content, "auto run_tests(std::string const& test)")
-	return_line = findNextReturnInTestSuite(test_suite_file_content, run_tests_line)
+		file.write("".join(cmake_file_data))
 
-	# Note: Conversion to index kept for clarity
-	run_test_start_index = (run_tests_line - 1) + 1
-	run_test_end_index   = (return_line - 1) - 1
+def updateTestSuiteFile(test_suite):
+	with open(test_suite + ".cpp", 'r') as file:
+		test_suite_data = file.readlines()
 
-	del test_suite_file_content[run_test_start_index:run_test_end_index+1]
-	test_suite_file_content[run_test_start_index:run_test_start_index] = [
+	# Find range of existing test macros
+	tests_start_index = findLineIndexOfMatch(test_suite_data, "auto run_tests(std::string const& test)") + 1
+	tests_end_index = findLineIndexOfNextReturn(test_suite_data, tests_start_index) - 1
+
+	# Replace existing test macros with tests found in source code
+	del test_suite_data[tests_start_index:tests_end_index+1]
+	test_suite_data[tests_start_index:tests_start_index] = [
 		"\tRUN_TEST_IF_ARGUMENT_EQUALS(" + x + ");\n"
 		for x in findAllTestsInFile(test_suite + ".cpp")
 	]
 
 	with open(test_suite + ".cpp", "w") as file:
-		file.write("".join(test_suite_file_content))
+		file.write("".join(test_suite_data))
+
+os.chdir(os.path.dirname(__file__))
+
+# Find all test suites
+test_suites = [ name[:-4] for name in glob.glob("test_*.cpp")]
+
+for test_suite in test_suites:
+	updateCMakeLists(test_suite)
+	updateTestSuiteFile(test_suite)
